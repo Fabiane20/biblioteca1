533ce0957d68f1ef14b8f4691c692ac4f94e256c
Fabiane Paulus <122945792+Fabiane20@users.noreply.github.com>
2026-03-10 19:31:01 -0300

    Update Biblioteca.java

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,7 +1,10 @@
 package exe1_1;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -12,30 +15,31 @@ public class Biblioteca {
     private String nome;
     private List<Livro> acervo;//lista que armazena os livros da biblio
 
-    public Biblioteca(String nome){
+    public Biblioteca(String nome) {
         this.nome = nome;
         acervo = new ArrayList<>();//cria lista vazia para armazenar os livros
     }
 
-    public void addLivro(Livro l){//metodo pra add um livro no acervo
+    public void addLivro(Livro l) {//metodo pra add um livro no acervo
         acervo.add(l);//add livro
     }
 
-    public void removerLivro(int id){//metodo para remover livro por id
+    public void removerLivro(int id) {//metodo para remover livro por id
 
-        for(int i = 0; i < acervo.size(); i++){//for para percorrer os livros da lista
+        for (int i = 0; i < acervo.size(); i++) {//for para percorrer os livros da lista
 
-            if(acervo.get(i).getId() == id){//if pra verificar se o id removido é mesmo o informado
+            if (acervo.get(i).getId() == id) {//if pra verificar se o id removido é mesmo o informado
                 acervo.remove(i);//se sim ele remove
                 break;
             }
         }
     }
 
-    public int verificaTotalLivros(){//metodo pra verificar a qauntidade total
+    public int verificaTotalLivros() {//metodo pra verificar a qauntidade total
         return acervo.size();
     }
-        // listar livros
+    // listar livros
+
     public void listarLivros() {
 
         for (Livro l : acervo) {//percorrer todos os livros do acervo 
@@ -47,5 +51,34 @@ public class Biblioteca {
             System.out.println("----------------------");
         }
     }
-}
 
+    public void listarAutoresPublicacoes() {
+
+        Map<String, Autor> mapaAutores = new HashMap<>();
+
+        // percorre os livros da biblioteca
+        for (Livro l : acervo) {
+
+            String nomeAutor = l.getAutor();
+
+            if (!mapaAutores.containsKey(nomeAutor)) {
+                mapaAutores.put(nomeAutor, new Autor(nomeAutor));
+            }
+
+            mapaAutores.get(nomeAutor).adicionarPublicacao();
+        }
+
+        // transforma o mapa em lista
+        List<Autor> listaAutores = new ArrayList<>(mapaAutores.values());
+
+        // ordena da maior quantidade para a menor
+        Collections.sort(listaAutores, (a1, a2)
+                -> Integer.compare(a2.getQuantidadePublicacoes(), a1.getQuantidadePublicacoes())
+        );
+
+        // imprime os autores
+        for (Autor a : listaAutores) {
+            System.out.println(a.getNome() + " - " + a.getQuantidadePublicacoes() + " publicações");
+        }
+    }
+}
